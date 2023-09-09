0be107a46c25470c4b705182896abb77b03f6095
JorgeSaicoski <jorgesaicoski@gmail.com>
2023-09-08 23:33:16 -0300

    404 not found agregate

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { NotFoundComponent } from './components/not-found/not-found.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' },
@@ -8,6 +9,7 @@ const routes: Routes = [
     loadChildren: () =>
       import('./modules/home/home.module').then((m) => m.HomeModule),
   },
+  { path: '**', component: NotFoundComponent },
 ];
 
 @NgModule({
