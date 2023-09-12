c0f548f76724f4a3e88e091951b7a2e814a95a9b
Jorge Saicoski <jorgesaicoski@gmail.com>
2023-09-12 00:14:03 -0300

    auth service angular

--- server/src/auth/auth.controller.ts
+++ server/src/auth/auth.controller.ts
@@ -1,5 +1,6 @@
 import { Body, Controller, HttpCode, HttpStatus, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
+import { User } from 'src/user/entities/user.entity';
 
 @Controller('auth')
 export class AuthController {
@@ -9,5 +10,16 @@ export class AuthController {
     @Post('login')
     signIn(@Body() signInDto: Record<string, any>){
         return this.authService.signIn(signInDto.username, signInDto.password);
+    }
+
+    @HttpCode(HttpStatus.OK)
+    @Post('register')
+    register(@Body() registerDto: Record<string, any>){
+        const user: User = new User()
+        user.name = registerDto.name
+        user.email = registerDto.email
+        user.username = registerDto.username
+        user.password = registerDto.password
+        return this.authService.register(user);
     }
 }
--- server/src/auth/auth.service.ts
+++ server/src/auth/auth.service.ts
@@ -1,22 +1,42 @@
 import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
 import { UserService } from '../user/user.service';
+import { User } from 'src/user/entities/user.entity';
 
 @Injectable()
 export class AuthService {
-    constructor(
-        private usersService:UserService,
-        private jwtService: JwtService
-    ){}
-    async signIn(username:string, pass:string){
-        const user = await this.usersService.findUserByUsername(username);
-        if (user?.password !== pass) {
-            throw new UnauthorizedException();
-          }
-          const payload = { sub: user.id, username: user.username, expireVipIn:user.expireVipIn };
-          return {
-            access_token: await this.jwtService.signAsync(payload),
-          };
+  constructor(
+    private usersService: UserService,
+    private jwtService: JwtService
+  ) { }
+  async signIn(username: string, password: string) {
+    try {
+      const user = await this.usersService.findUserByUsername(username);
 
+      if (!user || user.password !== password) {
+        throw new UnauthorizedException('Invalid credentials');
+      }
+
+      const payload = { sub: user.id, username: user.username, expireVipIn: user.expireVipIn };
+      const access_token = await this.jwtService.signAsync(payload);
+
+      return { access_token };
+    } catch (err) {
+      throw new UnauthorizedException('Invalid credentials');
     }
+  }
+
+  async register(user: User) {
+    try {
+      user = await this.usersService.createUser(user);
+      const payload = { sub: user.id, username: user.username, expireVipIn: user.expireVipIn };
+      const access_token = await this.jwtService.signAsync(payload);
+
+      return { access_token };
+    } catch (err) {
+      console.log(err)
+      throw new UnauthorizedException('Registration failed');
+    }
+  }
 }
+
