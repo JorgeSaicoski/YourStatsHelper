0f1c9cbb0c57b19a075f859f6d88fb7d72064450
Jorge Saicoski <jorgesaicoski@gmail.com>
2023-09-21 16:36:54 -0300

    redirect to buy vip if the client doesnt have it

--- client/src/app/guards/vip.guard.ts
+++ client/src/app/guards/vip.guard.ts
@@ -1,6 +1,5 @@
 import { inject } from '@angular/core';
 import { ActivatedRouteSnapshot, CanActivateFn, Router, RouterStateSnapshot, UrlTree } from '@angular/router';
-import { User } from '@model/user.model';
 import { TokenService } from '@service/auth/token.service';
 import { UsersService } from '@service/users/users.service';
 import { Observable, map } from 'rxjs';
@@ -14,14 +13,22 @@ export const vipGuard: CanActivateFn =  (route: ActivatedRouteSnapshot, state: R
   | UrlTree=> {
     const userService = inject(UsersService)
     const tokenService = inject(TokenService)
+    const routerService = inject(Router)
     const logged: boolean = tokenService.isAuthenticated()
+    console.log(logged)
     const id = tokenService.getIdByToken()
     if (id){
       return userService.getUserByID(id).pipe(
-        map(user => userService.checkVipIsValid(user) ? true : false)
-        );
+        map((user) => {
+          if (!userService.checkVipIsValid(user)) {
+            return routerService.createUrlTree(['/profile/vip-purchase']);
+          } else {
+            return true;
+          }
+        })
+      );
     } else{
-      return logged?inject(Router).createUrlTree(['/profile/vip-purchase']):inject(Router).createUrlTree(['/auth/login'])
+      return routerService.createUrlTree(['/auth/login'])
     }
     
 
