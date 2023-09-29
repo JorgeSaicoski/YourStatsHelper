df29ff0658437b645841460f1b09374b8c0dfd60
Jorge Saicoski <jorgesaicoski@gmail.com>
2023-09-29 15:17:14 -0300

    password fixed

--- server/src/auth/auth.service.ts
+++ server/src/auth/auth.service.ts
@@ -14,17 +14,18 @@ export class AuthService {
     try {
       const user = await this.usersService.findUserByUsername(username);
       if (!user){
-        console.log("not user")
         throw new NotFoundException('User not found');
       }
 
-      if (comparePasswords(password, user.password)) {
+      const isValidPassword: boolean = await comparePasswords(password, user.password);
+
+      if (isValidPassword) {
         const payload = { sub: user.id, username: user.username, expireVipIn: user.expireVipIn };
         const access_token = await this.jwtService.signAsync(payload);
         return { "access_token": access_token };
       }
 
-      throw new UnauthorizedException('Invalid credentials');
+      throw new UnauthorizedException('Invalid password');
       
     } catch (err) {
       throw err;
