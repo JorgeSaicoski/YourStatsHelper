31308240ec87100dc1e4e6256f9f4f745ec3a2f5
Jorge Saicoski <jorgesaicoski@gmail.com>
2023-10-02 17:14:17 -0300

    git ignore

--- client/src/environments/environment.ts
+++ client/src/environments/environment.ts
@@ -2,5 +2,5 @@ export const environment = {
     production: false,
     currentSesion: 'current_sesion',
     api_url: "http://127.0.0.0:3000/",
-    coinbase_api_key: "test"
+    coinbase_api_key: "test_api"
 }
