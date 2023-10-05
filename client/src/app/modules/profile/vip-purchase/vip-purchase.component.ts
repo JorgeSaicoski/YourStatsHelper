9f02362c225951c04345dd9846120e09ffda20e6
Jorge Saicoski <jorgesaicoski@gmail.com>
2023-10-05 00:23:59 -0300

    plans with better description

--- client/src/app/modules/profile/vip-purchase/vip-purchase.component.ts
+++ client/src/app/modules/profile/vip-purchase/vip-purchase.component.ts
@@ -23,31 +23,31 @@ export class VipPurchaseComponent implements OnInit {
   plans: Plan[] =[
     {
       id: 1,
-      name: "Month",
-      days: 30,
-      price: 5,
-      description: "Need help with a specific test that will be in the next week or soon?"
-    },
-    {
-      id: 2,
-      name: "Six Month",
-      days: 180,
-      price: 18,
-      description: "Need help in your semester? Wee will help you to pass in that."
-    },
-    {
-      id: 3,
-      name: "Year",
-      days: 365,
-      price: 24,
-      description: "Doesnt want to wast money? Buy here!"
-    },
-    {
-      id: 4,
       name: "Week",
       days: 7,
       price: 2.5,
-      description: "Wants to test? Come here!"
+      description: "Looking to test the waters? Dive into our Week Plan! It's perfect for those seeking a brief trial period."
+    },
+    {
+      id: 2,
+      name: "Month",
+      days: 30,
+      price: 5,
+      description: "Need assistance with an upcoming test or exam? Our Month Plan offers support tailored for your needs in the next month or soon."
+    },
+    {
+      id: 3,
+      name: "Six Month",
+      days: 180,
+      price: 18,
+      description: "Embarking on a semester-long journey? Our Six-Month Plan is designed to guide you through your entire semester and ensure your success."
+    },
+    {
+      id: 4,
+      name: "Year",
+      days: 365,
+      price: 24,
+      description: "Want to make a long-term commitment without breaking the bank? The Year Plan provides a cost-effective solution to help you achieve your academic goals."
     }
   ]
 
