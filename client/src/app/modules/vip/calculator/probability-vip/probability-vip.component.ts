fbd15d3d92f2a928ad18fcb5ca614721713742d4
Jorge Saicoski <jorgesaicoski@gmail.com>
2023-10-06 15:57:45 -0300

    probabilities to get in a row and improved html

--- client/src/app/modules/vip/calculator/probability-vip/probability-vip.component.ts
+++ client/src/app/modules/vip/calculator/probability-vip/probability-vip.component.ts
@@ -8,10 +8,12 @@ import { IMessage } from '@model/message.model';
   styleUrls: ['./probability-vip.component.scss']
 })
 export class ProbabilityVipComponent {
+
   possibilities:number = 1;
   numberOfEvents:number = 1;
   probability:number = 0;
   possibilities_wanted:number = 1;
+  kind:string = "get_number_once"
   message: IMessage ={
     show:false
   }
@@ -26,11 +28,32 @@ export class ProbabilityVipComponent {
      return;
     }
     const singlePossibility = this.possibilities_wanted/this.possibilities
-    console.log(Math.pow(-(singlePossibility-1), this.numberOfEvents))
-    this.probability = -(Math.pow(-(singlePossibility-1), this.numberOfEvents)-1) * 100;
-    if (this.probability < 0){
-      this.probability = -this.probability
 
+    switch(this.kind) { 
+        case "get_number_once": { 
+          this.probability = -(Math.pow(-(singlePossibility-1), this.numberOfEvents)-1) * 100 
+            break; 
+        } 
+        case "in_row": { 
+          this.probability = Math.pow(singlePossibility, this.numberOfEvents) * 100
+            break; 
+        } 
+        default: { 
+          this.probability = singlePossibility * 100
+            break; 
+        } 
+      }
+    ;
+    if (this.probability < 0){
+      this.message ={
+        show:true,
+        category:"error",
+        message:"You have used negatives values, please ignore the '-' signal"
+
+      } 
+
+    }else{
+      this.message.show = false
     }
   }
 
