d3cc9dbdab1cf27300f3c990fe829e542600ad9a
Jorge Saicoski <jorgesaicoski@gmail.com>
2023-10-10 17:03:15 -0300

    welcome page with pictures separeted by line

--- client/src/app/modules/home/welcome/welcome.component.ts
+++ client/src/app/modules/home/welcome/welcome.component.ts
@@ -11,5 +11,22 @@ export class WelcomeComponent {
     src:"assets/images/calculator.png",
     name:"Free Calculator",
     description:"Do statistics calculations for free."
-  }]
+  },
+  {
+    src:"assets/images/probability.png",
+    name:"Probability Calculator",
+    description:"Do probabilities calculations for 0.99USD."
+  },
+  {
+    src:"assets/images/vip.png",
+    name:"Explanation",
+    description:"Learn how to do and what means any results."
+  },
+  {
+    src:"assets/images/welcome.png",
+    name:"Welcome Page VIP",
+    description:"This is our central for VIPs users"
+  }
+
+]
 }
